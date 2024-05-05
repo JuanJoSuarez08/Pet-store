439073aa503b306c62bd7398a7c941ca6920fd80
Juan Jose <suarezorregojuanjose@gmail.com>
2024-05-04 21:00:27 -0500

    Website

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    // Inicializar loginForm en el constructor
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    // No es necesario inicializar loginForm en ngOnInit
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Lógica para autenticar al usuario
+      console.log(this.loginForm.value);
+    } else {
+      
+    }
+  }  
+}
